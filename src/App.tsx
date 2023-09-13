17ea98b3d3654e74922303a7cf2befaab16a63ba
FrowningCat <vsavinkov60@gmail.com>
2023-09-13 13:28:11 +0300

    added the ability to create things

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import {Product} from './components/Product'
 import {useProducts} from "./hooks/products"
 import {Loader} from "./components/Loader";
 import {ErrorMessage} from "./components/ErrorMessage";
+import {Model} from "./components/Model";
+import {CreateProduct} from "./components/CreateProduct";
+import {createElement, useState} from "react";
+import {IProduct} from "./models";
 
 function App() {
-    const {loading, error, products} = useProducts()
+    const {loading, error, products, addProduct} = useProducts()
+    const [model, setModel] = useState(true)
+
+    const createHandler = (product: IProduct) => {
+        setModel(false)
+        addProduct(product)
+    }
 
     return(
         <div className='container mx-auto max-w-2xl pt-5'>
             { loading && <Loader></Loader>}
             { error && <ErrorMessage error={error}></ErrorMessage> }
             { products.map(product => <Product product={product} key={product.id}/>) }
+
+            {model && <Model title='Create new product'>
+                <CreateProduct onCreate={createHandler}></CreateProduct>
+            </Model>}
         </div>
     )
 }
